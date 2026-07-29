66177faddb3b7100f322b4053a2a2f3965f8cf0c
Itz-Caseay <gred01485@gmail.com>
2026-07-29 10:18:35 +0100

    A calculator with python1.3.3

--- Day 2/fileOrganizer.py
+++ Day 2/fileOrganizer.py
@@ -1,15 +1,15 @@
-import os
-import shutil
+# import os
+# import shutil
 
-path = input("Enter Path: ")
-files = os.listdir(path)
+# path = input("Enter Path: ")
+# files = os.listdir(path)
 
-for file in files:
-    filename,extension = os.path.splitext(file)
-    extension = extension[1:]
+# for file in files:
+#     filename,extension = os.path.splitext(file)
+#     extension = extension[1:]
             
-    if os.path.exists(path+'/'+extension):
-        shutil.move(path+'/'+file, path+'/'+extension+'/'+file)
-    else:
-        os.makedirs(path+'/'+extension)
-        shutil.move(path+'/'+file, path+'/'+extension+'/'+file)
+#     if os.path.exists(path+'/'+extension):
+#         shutil.move(path+'/'+file, path+'/'+extension+'/'+file)
+#     else:
+#         os.makedirs(path+'/'+extension)
+#         shutil.move(path+'/'+file, path+'/'+extension+'/'+file)
