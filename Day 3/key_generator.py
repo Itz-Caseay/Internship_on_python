9b6540a6edad5fe8055219445fd5cfce3bb9c2c8
Itz-Caseay <gred01485@gmail.com>
2026-07-30 10:12:37 +0100

    A calculator with python1.4.0

--- Day 3/key_generator.py
+++
@@ -1,9 +0,0 @@
-import secrets
-import string
-
-# Generate a secure JWT secret
-jwt_secret = secrets.token_hex(32)
-jwt_refresh_secret = secrets.token_hex(32)
-
-print(f"JWT_SECRET={jwt_secret}")
-print(f"JWT_REFRESH_SECRET={jwt_refresh_secret}")
