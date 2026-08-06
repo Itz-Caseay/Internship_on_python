ac6fef4bb4f5b8d6b971b9474a4162d834c9a9f1
Itz-Caseay <gred01485@gmail.com>
2026-08-06 20:49:48 +0100

    latest2.1.8

--- Day 4/randomGrouping.py
+++ Day 4/randomGrouping.py
@@ -4,3 +4,14 @@ import random
 
 def generate_groups():
     # Get names from the text box
+    names = text_input.get("1.0", tk.END).strip().split("\n")
+    names == [name.strip() for name in names if name.strip()]
+    
+    if len(names)<3:
+        messagebox.showerror("Error": "Please enter atleast 3 names.")
+        return
+    
+    # shuffle names
+    random.shuffle(names)
+    
+    # create groups of 3
